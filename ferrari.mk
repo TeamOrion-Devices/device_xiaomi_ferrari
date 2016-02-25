c97ac6cc79bd47dc7bc5fb1190ef9867d888126f
Rishabh <rishabhad93@gmail.com>
2016-02-25 11:39:04 +0530

    Update ferrari.mk

--- ferrari.mk
+++ ferrari.mk
@@ -1,8 +1,3 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from ferrari device
-$(call inherit-product, device/xiaomi/ferrari/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
@@ -10,3 +5,9 @@ PRODUCT_NAME := ferrari
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 4i
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ferrari device
+$(call inherit-product, device/xiaomi/ferrari/device.mk)
