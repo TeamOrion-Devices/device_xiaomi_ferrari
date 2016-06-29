fc042695245231ad640adaf170816dcae74bdeea
Rishabh <rishabhad93@gmail.com>
2016-06-29 22:20:42 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 add_lunch_combo orion_ferrari-userdebug
-+add_lunch_combo orion_ferrari-user
-+add_lunch_combo orion_ferrari-eng
+add_lunch_combo orion_ferrari-user
+add_lunch_combo orion_ferrari-eng
